1dfd78f01ea8997fecebbfc0b13c66a0433567ef
jonolsu <jonathanbennett@yahoo.com>
2016-06-07 17:15:50 -0500

    first try of createshapefile

--- shapefileutilities.R
+++ shapefileutilities.R
@@ -1,11 +1,12 @@
-createshapefile(inputfilepath="c:/temp/Block_Vertices.csv")
-
-createshapefile <- function(inputfilepath)
+createshapefile <- function(inputfilepath, outputfilepath)
 {
     library(shapefiles)
     df <- read.csv(inputfilepath)
-    longformat <- getlongformat(df)
-    
+    dd <- getlongformat(df)
+    ddTable <- df[,c(1,1)]
+    names(ddTable) <- c("Id","Name")
+    ddShapefile <- shapefiles::convert.to.shapefile(dd,ddTable,"Id",5)
+    write.shapefile(ddShapefile,outputfilepath,arcgis=T)
 }
 
 getlongformat <- function(df)
@@ -13,10 +14,14 @@ getlongformat <- function(df)
     dfout <- data.frame(Id=character(0),X=numeric(0),Y=numeric(0))
     for(i in 1:nrow(df))
     {
-        dfout <- rbind(dfout,df[i,c(1,2:3)])
-        dfout <- rbind(dfout,df[i,c(1,4:5)])
-        dfout <- rbind(dfout,df[i,c(1,6:7)])
-        dfout <- rbind(dfout,df[i,c(1,8:9)])
+        for(j in 1:4)
+        {
+            newrow <- df[i,c(1,(j*2):(j*2+1))]
+            names(newrow) <- c("Id","X","Y")
+            dfout <- rbind(dfout,newrow)
+        }
     }
     dfout
 }
+
+#createshapefile(inputfilepath="c:/temp/Block_Vertices.csv")
